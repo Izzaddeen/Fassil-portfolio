01c0a9aad46f99dba43c6b05a9225b63c1d18493
Izzaddeen <izzarex05@gmail.com>
2024-08-02 18:10:44 +0530

    DONE

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   DCV.addEventListener('click', function () {
       const downloadLink = document.createElement('a');
-      downloadLink.href = 'cv/Fassil.pdf'; 
+      downloadLink.href = 'CV/Fassil.pdf'; 
       downloadLink.download = 'Fassil.pdf'; 
       document.body.appendChild(downloadLink);
       downloadLink.click();
